35f13bdf6b1db7d55413ea33d2c7c8678279ebc8
Lais Rhianne <lays.rhianne@gmail.com>
2021-06-22 20:23:21 -0300

    routes implemented on server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 
+import { router } from './routes';
+
 import './database';
 
 const app = express();
+
+app.use(router);
 
 app.listen(3333, () => console.log('Server running on port 3000'));
