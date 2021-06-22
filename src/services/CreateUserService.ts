bbf2a1fb33ae41f937bd3f54c295eee3498624cb
Lais Rhianne <lays.rhianne@gmail.com>
2021-06-22 20:12:12 -0300

    service createUser added

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,30 @@
+import { UsersRepositories } from '../repositories/UsersRepositories';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const UsersRepository = new UsersRepositories();
+
+    if (!email) {
+      throw new Error('Email is required');
+    }
+
+    const userAlreadyExists = await UsersRepository.findOne({ email });
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const user = UsersRepository.create({ email, name, admin });
+
+    await UsersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
