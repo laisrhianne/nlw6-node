b64929f54f1de50a0e1ba6d3ee9407a877a5274a
Lais Rhianne <lays.rhianne@gmail.com>
2021-06-23 20:16:57 -0300

    tratamento de erros adicionado

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import { router } from './routes';
 
@@ -11,4 +12,12 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(3333, () => console.log('Server running on port 3000'));
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({ error: err.message });
+  }
+
+  return response.status(500).json({ status: 'error', message: 'Internal Server Error' });
+});
+
+app.listen(3333, () => console.log('Server running on port 3333'));
