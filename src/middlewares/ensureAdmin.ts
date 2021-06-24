c5409d831e7e1e0decb310536b02343dde7b9bc6
Lais Rhianne <lays.rhianne@gmail.com>
2021-06-24 07:35:52 -0300

    middleware ensureAdmin added

---
+++ src/middlewares/ensureAdmin.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+  // Verificar se usuário é admin
+  const admin = true;
+
+  if (admin) {
+    next();
+  }
+
+  return response.status(401).json({ error: 'Unauthorized' });
+}
