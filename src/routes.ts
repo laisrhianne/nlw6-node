dc22feec36ac3dde0ea33d4d0ec661a6dd533118
Lais Rhianne <lays.rhianne@gmail.com>
2021-06-26 18:19:06 -0300

    list tags and users added

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,8 @@ import { CreateComplimentController } from './controllers/CreateComplimentContro
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { ListUserSentComplimentsController } from './controllers/ListUserSentComplimentsController';
 import { ListUserReceivedComplimentsController } from './controllers/ListUserReceivedComplimentsController';
+import { ListTagsController } from './controllers/ListTagsController';
+import { ListUsersController } from './controllers/ListUsersController';
 
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
@@ -18,6 +20,8 @@ const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
 const listUserSentComplimentsController = new ListUserSentComplimentsController();
 const listUserReceivedComplimentsController = new ListUserReceivedComplimentsController();
+const listTagsController = new ListTagsController();
+const listUsersController = new ListUsersController();
 
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
@@ -25,5 +29,7 @@ router.post('/login', authenticateUserController.handle);
 router.post('/compliments', ensureAuthenticated, createComplimentController.handle);
 router.get('/users/compliments/sent', ensureAuthenticated, listUserSentComplimentsController.handle);
 router.get('/users/compliments/received', ensureAuthenticated, listUserReceivedComplimentsController.handle);
+router.get('/tags', ensureAuthenticated, listTagsController.handle);
+router.get('/users', ensureAuthenticated, listUsersController.handle);
 
 export { router };
