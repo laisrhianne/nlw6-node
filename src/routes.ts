da01c9ef10b8c739917782c5a957e531589d67cd
Lais Rhianne <lays.rhianne@gmail.com>
2021-06-26 10:20:05 -0300

    compliments added

--- src/routes.ts
+++ src/routes.ts
@@ -5,15 +5,18 @@ import { CreateTagController } from './controllers/CreateTagController';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 
 import { ensureAdmin } from './middlewares/ensureAdmin';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
 router.post('/login', authenticateUserController.handle);
+router.post('/compliments', createComplimentController.handle);
 
 export { router };
